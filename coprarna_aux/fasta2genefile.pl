0058970560f00fa36ba0e1f6cf220cb3d30e2cba
martin-mann <qyu@gmx.de>
2018-08-08 14:42:43 +0200

    documentation added

--- coprarna_aux/fasta2genefile.pl
+++ coprarna_aux/fasta2genefile.pl
@@ -1,31 +1,48 @@
 #!/usr/bin/env perl
+# usage : fasta2genefile.pl <FASTAFILE>
+# produces FASTAFILE.gene and FASTAFILE.tit from FASTA header information
+
 $infile = $ARGV[0];
 $outname = $infile if (! $outname);
-open(IN, $infile) || die;
-open(GENE, ">$outname.gene") || die;
-open(TIT, ">$outname.tit") || die;
+
+# open streams or die trying
+open(IN, $infile) || die("fasta2genefile.pl : cannot open/read from $infile");
+open(GENE, ">$outname.gene") || die("fasta2genefile.pl : cannot  write to $outname.gene");
+open(TIT, ">$outname.tit") || die("fasta2genefile.pl : cannot  write to $outname.tit");
+
+# process input FASTA file
 while(<IN>){
 	chomp;
+	# get FASTA id
 	if (/^>\s*(\S.*)/) {
+		# split fasta id in name (up to first whitespace) and rest (=title)
 		($name0,$title0) = split(/\s+/, $1, 2);
+		# if sequence was already parsed (last record) -> print
 		if ($seq) {
 			($sp, $gene) = split(/:/, $name);
 			$seqlen = length($seq);
 			print GENE "$sp $gene $seqlen\n";
 			print TIT "$sp:$gene\t$title\n";
 		}
+		# set data for this (new) record
 		$name = $name0;
 		$title = $title0;
+		# reset sequence
 		$seq = '';
 	} else {
+		# parse sequence (concatenate subsequent lines)
 		s/\s//g;
 		$seq .= $_;
 	}
 }
-close(IN);
+# handle last sequence
 if ($seq) {
 	($sp, $gene) = split(/:/, $name);
 	$seqlen = length($seq);
 	print GENE "$sp $gene $seqlen\n";
 	print TIT "$sp:$gene\t$title\n";
 }
+# close streams
+close(TIT);
+close(GENE);
+close(IN);
