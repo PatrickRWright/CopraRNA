c32d46910e7d7b9a9061d48c7d089f0ebb9cb51b
Martin Raden <mmann@informatik.uni-freiburg.de>
2019-10-09 20:06:37 +0200

    edit comments removed

--- coprarna_aux/prepare_intarna_out.pl
+++ coprarna_aux/prepare_intarna_out.pl
@@ -5,7 +5,7 @@ use warnings;
 
 use Parallel::ForkManager;
 use List::MoreUtils qw(uniq);
-use Cwd 'abs_path'; ## edit 2.0.5.1
+use Cwd 'abs_path'; 
 
 my $ncrnas = $ARGV[0];
 my $upfromstartpos = $ARGV[1]; 
@@ -16,37 +16,37 @@ my $refseqid = '';
 my $orgcnt = (scalar(@ARGV) - 4);
 
 # get absolute path
-my $ABS_PATH = abs_path($0); ## edit 2.0.5.1
+my $ABS_PATH = abs_path($0); 
 # remove script name at the end
 # match all non slash characters at the end of the string
-$ABS_PATH =~ s|[^/]+$||g; ## edit 2.0.5.1
+$ABS_PATH =~ s|[^/]+$||g; 
 my $PATH_COPRA_SUBSCRIPTS = $ABS_PATH;
 
 # get core count from option file
-my $cores = `grep 'core count:' CopraRNA_option_file.txt | grep -oP '\\d+'`; ## edit 2.0.4
-chomp $cores; ## edit 2.0.4
+my $cores = `grep 'core count:' CopraRNA_option_file.txt | grep -oP '\\d+'`; 
+chomp $cores;
 
 # check for verbose printing
-my $verbose = `grep 'verbose:' CopraRNA_option_file.txt | sed 's/verbose://g'`; ## edit 2.0.5.1
+my $verbose = `grep 'verbose:' CopraRNA_option_file.txt | sed 's/verbose://g'`; 
 chomp $verbose;
 
 # check if CopraRNA1 prediction should be made
-my $cop1 = `grep 'CopraRNA1:' CopraRNA_option_file.txt | sed 's/CopraRNA1://g'`; ## edit 2.0.5.1
+my $cop1 = `grep 'CopraRNA1:' CopraRNA_option_file.txt | sed 's/CopraRNA1://g'`; 
 chomp $cop1;
 
 # get window size option
-my $winsize = `grep 'win size:' CopraRNA_option_file.txt | sed 's/win size://g'`; ## edit 2.0.5.1
+my $winsize = `grep 'win size:' CopraRNA_option_file.txt | sed 's/win size://g'`; 
 chomp $winsize;
 
 # get maximum base pair distance 
-my $maxbpdist = `grep 'max bp dist:' CopraRNA_option_file.txt | sed 's/max bp dist://g'`; ## edit 2.0.5.1
+my $maxbpdist = `grep 'max bp dist:' CopraRNA_option_file.txt | sed 's/max bp dist://g'`; 
 chomp $maxbpdist;
 
 for(my $i=4;$i<=scalar(@ARGV) - 1;$i++) {
 
     my @splitarg = split(/,/, $ARGV[$i]);
 
-    if ($splitarg[0] =~ m/(N[ZC]_.+?)\.gb/) { ## edit 2.0.2
+    if ($splitarg[0] =~ m/(N[ZC]_.+?)\.gb/) { 
         $refseqid = $1;
     }
 
@@ -55,9 +55,9 @@ for(my $i=4;$i<=scalar(@ARGV) - 1;$i++) {
     my $splitargcount = 1;
 
     foreach (@splitarg) {
-        my $tempOutfile = $outfile; ## edit 1.2.2
-        $tempOutfile = $tempOutfile . $splitargcount; ## edit 1.2.2
-        system $PATH_COPRA_SUBSCRIPTS . "parse_region_from_genome.pl $_ $upfromstartpos $down $mrnapart > $tempOutfile"; ## edit 1.2.2
+        my $tempOutfile = $outfile; 
+        $tempOutfile = $tempOutfile . $splitargcount; 
+        system $PATH_COPRA_SUBSCRIPTS . "parse_region_from_genome.pl $_ $upfromstartpos $down $mrnapart > $tempOutfile"; 
         $splitargcount++;   
     }
 }
@@ -67,7 +67,7 @@ my @files = ();
 my @rfids = ();
 
 foreach (@files) {
-    if ($_ =~ m/(N[ZC]_.+?)\.gb/) { # edit 2.0.2
+    if ($_ =~ m/(N[ZC]_.+?)\.gb/) { 
         push (@rfids, $1)
     }
 }
@@ -89,7 +89,7 @@ my $switch = 0;
 my @ncrnaarray = (); 
 
 # preparing individual sRNA files for IntaRNA whole genome predictions
-open(MYDATA, $ncrnas) or die("\nError: cannot open file $ncrnas in prepare_intarna_out.pl\n\n"); ## edit 2.0.5.1 // added script name
+open(MYDATA, $ncrnas) or die("\nError: cannot open file $ncrnas in prepare_intarna_out.pl\n\n"); 
     my @ncrnalines = <MYDATA>;
 close MYDATA;
 
@@ -117,14 +117,14 @@ my $suffix = '_upfromstartpos_' . $upfromstartpos . '_down_' . $down . '.fa';
 
 my $pm = new Parallel::ForkManager($cores);   
 foreach (@files) {
-    if ($_ =~ m/(N[ZC]_.+)$suffix$/) { ## edit 2.0.2
-        my $refid = $1;  # get refseq id ## edit 2.0.2
+    if ($_ =~ m/(N[ZC]_.+)$suffix$/) { 
+        my $refid = $1;  # get refseq id
         foreach my $line (@ncrnalines) {
             if ($switch) {                  
                 push(@ncrnaarray, $line);   
                 $switch = 0;                
             }                               
-            if($line =~ m/$refid/) { ## edit 2.0.2
+            if($line =~ m/$refid/) {
                 $switch = 1;
                 push(@ncrnaarray, $line);
             }
@@ -133,10 +133,9 @@ foreach (@files) {
 		my $ncrnafilename1 = $ncrnaarray[0];
 			$ncrnafilename = $ncrnafilename . ".fa";
 			@ncrnaarray = ();
-		system("mkdir $ncrnafilename1"); # edit jens
-        my $intarnaout = $_ . ".intarna.csv"; ## edit 2.0.5.1 // added .csv
+		system("mkdir $ncrnafilename1");
+        my $intarnaout = $_ . ".intarna.csv"; 
             $pm->start and next;            
-            #system("/home/jens/For_CopraRNA2.0/IntaRNA-dev190704 --model=X --tAccW $winsize --tAccL $maxbpdist  --target $_ --query $ncrnafilename --outCsvCols 'id1,id2,seq1,seq2,subseq1,subseq2,subseqDP,subseqDB,start1,end1,start2,end2,hybridDP,hybridDB,E,ED1,ED2,Pu1,Pu2,E_init,E_loops,E_dangleL,E_dangleR,E_endL,E_endR,seedStart1,seedEnd1,seedStart2,seedEnd2,seedE,seedED1,seedED2,seedPu1,seedPu2,E_norm' --outMode=C --out $intarnaout --out=SpotProb:./$ncrnafilename1/probs.csv") unless (-e $intarnaout); ## edit 2.0.4 added -s 1 for consensus prediction // ## edit 2.0.5 changed to IntaRNA2 // ## edit 2.0.5.1 changed to IntaRNA 2.0 csv output
             system("IntaRNA --outOverlap=Q --tAccW $winsize --tAccL $maxbpdist  --target $_ --query $ncrnafilename --outCsvCols 'id1,id2,seq1,seq2,subseq1,subseq2,subseqDP,subseqDB,start1,end1,start2,end2,hybridDP,hybridDB,E,ED1,ED2,Pu1,Pu2,E_init,E_loops,E_dangleL,E_dangleR,E_endL,E_endR,seedStart1,seedEnd1,seedStart2,seedEnd2,seedE,seedED1,seedED2,seedPu1,seedPu2,E_norm' --outMode=C --out $intarnaout --out=SpotProb:./$ncrnafilename1/probs.csv") unless (-e $intarnaout);
 			print("IntaRNA --tAccW $winsize --tAccL $maxbpdist --outNumber 2 --target $_ --query $ncrnafilename --outCsvCols 'id1,id2,seq1,seq2,subseq1,subseq2,subseqDP,subseqDB,start1,end1,start2,end2,hybridDP,hybridDB,E,ED1,ED2,Pu1,Pu2,E_init,E_loops,E_dangleL,E_dangleR,E_endL,E_endR,seedStart1,seedEnd1,seedStart2,seedEnd2,seedE,seedED1,seedED2,seedPu1,seedPu2,E_norm' --outMode=C --out $intarnaout\n") if ($verbose);
             $pm->finish;
@@ -146,17 +145,17 @@ foreach (@files) {
 $pm->wait_all_children;  
 
 ## sort IntaRNA output by energy
-@files = <*intarna.csv>; ## edit 2.0.4 // added *csv // ## edit 2.0.5.1 // changed to *intarna.csv
+@files = <*intarna.csv>;
 
 foreach (@files) {
-    ## edit 2.0.4 // removed pattern match for files
+    
     my $temp = $_;
     chomp $temp;
     chop $temp;
     chop $temp;
     chop $temp;
     my $sortedcsv = $temp . "sorted.csv"; 
-    system "env LC_ALL=C sort -t';' -g -k15 $_ -o $sortedcsv"; ## edit 2.0.5.1 // removed sort_intarna_csv_results.pl and switched to linux sort
+    system "env LC_ALL=C sort -t';' -g -k15 $_ -o $sortedcsv"; 
 }
 
 @files = ();
@@ -174,18 +173,18 @@ my @datalines = ();
 # mixture of optimal and suboptimals
 
 ## creates *_opt.intarna.csv and *_subopt.intarna.csv files
-system $PATH_COPRA_SUBSCRIPTS . "disentangle_sorted_intarna_CSV.pl"; ## edit 2.0.4
+system $PATH_COPRA_SUBSCRIPTS . "disentangle_sorted_intarna_CSV.pl"; 
 
 ## adds pvalues to *_opt.intarna.csv and *_subopt.intarna.csv files // file names stay the same
-system "R --slave -f " . $PATH_COPRA_SUBSCRIPTS . "add_pval_to_csv_evdfit.R"; ## edit 2.0.4 // changed this to an R script
+system "R --slave -f " . $PATH_COPRA_SUBSCRIPTS . "add_pval_to_csv_evdfit.R"; 
                                                                               ## makes add_pval_to_csv_evdfit.pl obsolete
 ## create opt_tags.clustered
-system $PATH_COPRA_SUBSCRIPTS . "cluster_intarna_csv.pl > opt_tags.clustered"; ## edit 2.0.4.1 // reimplemented cluster_intarna_csv.pl from hash_clusters
+system $PATH_COPRA_SUBSCRIPTS . "cluster_intarna_csv.pl > opt_tags.clustered"; 
 
 ## create opt_tags.clustered_rcsize
-system "R --slave -f " . $PATH_COPRA_SUBSCRIPTS . "remove_clusters_under_percantage.R" if ($cop1); ## edit 2.0.1
+system "R --slave -f " . $PATH_COPRA_SUBSCRIPTS . "remove_clusters_under_percantage.R" if ($cop1); 
 
 system "mafft --localpair --quiet 16s_sequences.fa > 16s_sequences.aln";
-system "distmat -sequence 16s_sequences.aln -nucmethod 1 -outfile distmat.out 2> /dev/null"; ## edit 2.0.5.1 // added 2> /dev/null to prevent output to the terminal
+system "distmat -sequence 16s_sequences.aln -nucmethod 1 -outfile distmat.out 2> /dev/null"; 
 system $PATH_COPRA_SUBSCRIPTS . "transform_distmat.pl distmat.out > compatible.distmat";
 
