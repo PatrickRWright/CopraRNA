dce9ccc5a9046f0b0856400278fb40dd08f37a01
Martin Mann <mmann@bipc35.informatik.uni-freiburg.de>
2019-10-17 10:40:38 +0200

    test script

--- test/test1.sh
+++ test/test1.sh
@@ -1,5 +1,34 @@
-# standard test call 
-CopraRNA2.pl -srnaseq sRNAs.fa -ntup 200 -ntdown 100 -region 5utr -enrich 200 -topcount 200 -cores 4
+#!/usr/bin/env bash
 
-# standard test call noclean
-CopraRNA2.pl -srnaseq sRNAs.fa -ntup 200 -ntdown 100 -region 5utr -enrich 200 -topcount 200 -cores 4 --noclean
+##########################################################
+
+# default: remove temporary files at end of call
+KEEPTMPFILES=
+# comment next line to remove temporary files at end of cal
+KEEPTMPFILES="--noclean"
+
+TESTDIR=$PWD
+
+##########################################################
+
+CALLARGS=" -verbose -srnaseq $TESTDIR/test1.fa -ntup 200 -ntdown 100 -region 5utr -enrich 200 -topcount 200 -cores 4 $KEEPTMPFILES"
+
+##########################################################
+
+TESTTMPDIR=test1-tmp
+echo "create and enter test directory '$TESTDIR/$TESTTMPDIR'"
+rm -rf $TESTTMPDIR
+mkdir -p $TESTTMPDIR
+cd $TESTTMPDIR
+
+echo "run test call : $CALLARGS"
+../../CopraRNA2.pl $CALLARGS
+
+# leave test directory
+cd $TESTDIR
+
+echo "end of test: exit code = $?"
+
+# exit test with test's exit code
+exit $?
+
