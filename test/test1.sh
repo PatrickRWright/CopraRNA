13ccd04b8f18d63462732d1580f9c5271244c811
Martin Mann <mmann@bipc35.informatik.uni-freiburg.de>
2019-10-18 13:01:23 +0200

    test based on dummy genomes

--- test/test1.sh
+++ test/test1.sh
@@ -8,14 +8,15 @@ KEEPTMPFILES=
 KEEPTMPFILES="--noclean"
 
 TESTDIR=$PWD
+TESTFILE=$0
 
 ##########################################################
 
-CALLARGS=" -verbose -srnaseq $TESTDIR/test1.fa -genomePath $TESTDIR/genomes -ntup 200 -ntdown 100 -region 5utr -enrich 200 -topcount 200 -cores 4 $KEEPTMPFILES"
+CALLARGS=" -verbose -srnaseq $TESTDIR/test1.fa -genomePath $TESTDIR/$TESTFILE-genomes -ntup 200 -ntdown 100 -region 5utr -enrich 200 -topcount 200 -cores 4 $KEEPTMPFILES"
 
 ##########################################################
 
-TESTTMPDIR=test1-tmp
+TESTTMPDIR=$TESTFILE-tmp
 echo "create and enter test directory '$TESTDIR/$TESTTMPDIR'"
 rm -rf $TESTTMPDIR
 mkdir -p $TESTTMPDIR
@@ -23,12 +24,24 @@ cd $TESTTMPDIR
 
 echo "run test call : $CALLARGS"
 ../../CopraRNA2.pl $CALLARGS
+CALLEXITCODE=$?
+echo "exit code of call = $CALLEXITCODE"
 
 # leave test directory
 cd $TESTDIR
 
-echo "end of test: exit code = $?"
+
+if [ "$CALLEXITCODE" -eq "0" ]; then
+	DIFFOUT=$TESTFILE-result.diff
+	diff $TESTTMPDIR/CopraRNA_result_all.csv $TESTFILE-results/CopraRNA_result_all.csv > $DIFFOUT;
+	if [ -s "$DIFFOUT" ]; then
+		echo "TEST FAILURE: check $DIFFOUT for details!";
+		exit -1;
+	else
+		echo "TEST SUCCESSFUL!";
+	fi
+fi
 
 # exit test with test's exit code
-exit $?
+exit $CALLEXITCODE
 
