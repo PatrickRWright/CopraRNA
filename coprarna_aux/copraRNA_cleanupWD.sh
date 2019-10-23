f283972a0ff8fe5cc23779c1b39a7ac01a2e5494
Martin Raden <mmann@informatik.uni-freiburg.de>
2019-10-23 11:42:58 +0200

    dedicated cleanup script

---
+++ coprarna_aux/copraRNA_cleanupWD.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+###################################################################
+# cleans a CopraRNA job directy after the job was run, i.e.
+# - removes temporary files
+# - aggregates input and output files in respective subfolders
+###################################################################
+
+rm -f *.gb *.gb.gz;
+rm -f compatible.distmat.mapped;
+rm -f *anno* padj.csv;
+rm -f *ncRNA*;
+rm -f *pvalues*;
+rm -f *.fa.intarna.sorted.csv *opt.intarna.csv;
+rm -f gene_CDS_exception.txt find_gaps.txt distmat.out;
+rm -f merged_refseq_ids.txt;    
+rm -f CopraRNA2_prep*;
+rm -f fasta_temp_file fasta_temp_file_out;
+find -regex ".*fa[0-9]+$" | xargs rm -f;
+rm -f ncrna_aligned.fa;
+rm -f weights.warning;
+rm -f aligned_sRNA.fa;
+rm -rf target_alignments;
+rm -f cluster_backup.tab;
+rm -f aux_table.csv;
+
+
+# blast files
+rm -f all.fas*;
+rm -f duplicated_CDS.txt;
+
+# enrichment
+rm -f enrichment.txt *DAVID* IntaRNA* intarna*;
+rm -f *IntaRNA1_ui* *top_targets*;
+
+# v1 specific
+rm -f *pvsample*;
+rm -f CopraRNA1_final_all.csv CopraRNA1_final.csv;
+
+############### subdir distribution ######################
+
+# log files for debug
+mkdir -p log;
+mv CopraRNA2_subprocess.* err.log error.log formatdb.log log/.;
+
+# make subdirs for IntaRNA, FASTA, Enrichment, Phylogeny and regions plots
+mkdir -p IntaRNA;
+mv *.fa.intarna.csv IntaRNA;
+
+mkdir -p Phylogeny;
+mv compatible.* Phylogeny;
+mv 16s_sequences* Phylogeny;
+
+mkdir -p FASTA;
+mv *.fa FASTA;
+mv -f FASTA/input_sRNA.fa .; # move input file back to root folder
+
+mkdir -p Regions_plots;
+mv *regions* Regions_plots;
+for f in thumbnail_*; do
+	mv $f Regions_plots;
+end
+
+if [ -s copra_heatmap.html]; then 
+    mkdir -p Enrichment;
+    mv copra_heatmap.html Enrichment;
+    mv copraRNA.json Enrichment;
+    mv enriched_heatmap_big* Enrichment;
+    mv termClusterReport.txt Enrichment;
+    rm -f org_of_interest_aux_enrichment.txt;
+    mv aux_table.csv Enrichment;
+fi
+
+# make an archive for the Rdata files
+ if [ -s int_sites.Rdata]; then
+    mkdir Rdata;
+    mv int_sites.Rdata Rdata;
+    mv order_table_all.Rdata Rdata;
+    mv conservation_table.Rdata Rdata;
+fi
+ 
+# zip evo_alignments folder to reduce file number
+if [ -d evo_alignments ]; then 
+	zip -rmq evo_alignments evo_alignments 2>&1; 
+fi;
+
+
+
+
