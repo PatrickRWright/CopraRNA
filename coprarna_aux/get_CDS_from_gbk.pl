52802d1121fe231d12f5256de5adf86c82903c3f
fayyazh <fayyaz.hussain@gmail.com>
2020-10-08 17:09:37 +0200

    seq in all.fas with keggcodes only

--- coprarna_aux/get_CDS_from_gbk.pl
+++ coprarna_aux/get_CDS_from_gbk.pl
@@ -67,31 +67,30 @@ my $seqin = Bio::SeqIO->new( -format => 'genbank', -fh => $fileHandle );
 # alternatively: check sub_SeqFeature of $sf  https://metacpan.org/pod/Bio::Seq
 my %printedCDS=();
 
-while( (my $seq = $seqin->next_seq()) ) {
+if ($keggcode ne "") {
+	while( (my $seq = $seqin->next_seq()) ) {
+		foreach my $sf ( $seq->get_SeqFeatures() ) {
 
-    foreach my $sf ( $seq->get_SeqFeatures() ) {
+			if( $sf->primary_tag eq 'CDS' 
+				and $sf->has_tag("locus_tag") 
+				and $sf->has_tag("translation")) {
+					# get locus tag
+					my @ltaglist = $sf->get_tag_values("locus_tag");
+					my $ltag = $ltaglist[0];
+					chomp $ltag;
 
-        if( $sf->primary_tag eq 'CDS' 
-			and $sf->has_tag("locus_tag") 
-			and $sf->has_tag("translation")) 
-		{
-				# get locus tag
-                my @ltaglist = $sf->get_tag_values("locus_tag");
-                my $ltag = $ltaglist[0];
-                chomp $ltag;
-
-				# print if unknown
-				if ( not exists($printedCDS{$ltag}) ) {
-					# get CDS data for entry
-					my @translationlist = $sf->get_tag_values("translation");
-					my $protein = $translationlist[0];
-					chomp $protein;
-					# print fasta entry
-					print ">$keggcode:$ltag\n$protein\n";
-					# mark as printed
-					$printedCDS{$ltag} = $ltag;
+					# print if unknown
+					if ( not exists($printedCDS{$ltag}) ) {
+						# get CDS data for entry
+						my @translationlist = $sf->get_tag_values("translation");
+						my $protein = $translationlist[0];
+						chomp $protein;
+						# print fasta entry
+						print ">$keggcode:$ltag\n$protein\n";
+						# mark as printed
+						$printedCDS{$ltag} = $ltag;
+					}
 				}
-      }
-   }
+		}
+	}
 }
-
