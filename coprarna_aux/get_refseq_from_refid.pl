09d64507bc4accd0999135c6b3ef1845f41d9386
fayyazh <fayyaz.hussain@gmail.com>
2020-10-08 17:24:48 +0200

    multi-core ftp download of missing organisms, 2nd option eutlities

--- coprarna_aux/get_refseq_from_refid.pl
+++ coprarna_aux/get_refseq_from_refid.pl
@@ -6,16 +6,29 @@ use Bio::SeqIO;
 use Bio::DB::EUtilities;
 use Getopt::Long;
 
+use Cwd 'abs_path'; 
+
 my $acc_number;             # accession number of query
 my $genome_file;            # genome file
+my $cores;
+my $genomePath;
 
 GetOptions('acc=s'      =>  \$acc_number,
-           'g|genome=s' =>  \$genome_file,);
+           'g|genome=s' =>  \$genome_file,
+	   'c=s'	=>  \$cores,
+	   'gPath=s'	=>  \$genomePath,);
 
 &usage unless ($acc_number && $genome_file);
 
 # check if file is already present
 exit(0) if (-e $genome_file);
+
+# get absolute path
+my $ABS_PATH = abs_path($0); 
+# remove script name at the end
+# match all non slash characters at the end of the string
+$ABS_PATH =~ s|[^/]+$||g; 
+my $PATH_COPRA_SUBSCRIPTS = $ABS_PATH;
 
 # get GI for given genome accession number
 my $factory = Bio::DB::EUtilities->new(-eutil => 'esearch',
@@ -42,23 +55,8 @@ while (my $item = $ds->next_Item('flattened'))  {
         if ($item->get_content && $item->get_name =~ /Length/);
 }
 
-# download the full genbank file to a file (not retained in memory)
-$factory = Bio::DB::EUtilities->new(-eutil => 'efetch',
-                                    -db => 'nuccore',
-                                    -id => $id,
-                                    -tool => 'coprarna',
-                                    -email => 'rna@informatik.uni-freiburg.de',
-                                    -rettype => 'gbwithparts'); # gbwithparts to get full sequence
-# pipe download into file (and compress if needed)
-if ($genome_file =~ m/(.+)\.gz$/) {
-	$factory->get_Response(-file => "$1"); 
-	system("gzip -9 $1");
-} else {
-	$factory->get_Response(-file => $genome_file); 
-}
-
-
-#####################################################################################################################################
+my $dwnCall = "python ". $PATH_COPRA_SUBSCRIPTS  . "download_genomes.py -g $acc_number -c $cores -p $genomePath/";
+system $dwnCall;
 
 sub usage {
    die("\nUsage: ./get_refseq_from_refid.pl -acc accession-number -g genome-file \n\n");
