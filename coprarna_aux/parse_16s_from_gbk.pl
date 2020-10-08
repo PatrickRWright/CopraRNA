e1f5bf14b1ca80f838b44a1881c7f1475b0b3ef2
fayyazh <fayyaz.hussain@gmail.com>
2020-10-08 17:28:36 +0200

    parallel processing incorporated

--- coprarna_aux/parse_16s_from_gbk.pl
+++ coprarna_aux/parse_16s_from_gbk.pl
@@ -7,12 +7,15 @@ use IO::File;
 use IO::Uncompress::Gunzip qw(gunzip $GunzipError);
 # genbank file parsing
 use Bio::SeqIO;
+use Parallel::ForkManager;
 
 # parses 16s rRNA sequences from genbank files
+my $cores = $ARGV[0];
 
-foreach(@ARGV) {
+my $pm = new Parallel::ForkManager($cores);
 
-    my @splitargv = split(/,/, $_);
+for (my $i=1;$i<scalar(@ARGV);$i++) {
+    my @splitargv = split(/,/, $ARGV[$i]);
 
     # save splitiargv[0] and put this in as header. remove the .gb !!
     my $MainRefID = $splitargv[0];
@@ -23,8 +26,9 @@ foreach(@ARGV) {
        die("\n parse_16s_from_gbk.pl : given genome file does not end in '.gb' or '.gb.gz'\n\n");
     }
 
-
+    $pm->start and next;    
     for (my $i=0; $i<scalar(@splitargv); $i++) {
+
 	my $genomeFile = $splitargv[$i];
     my $fileHandle = undef;
     if ($genomeFile =~ m/.+\.gz$/) {
@@ -53,5 +57,8 @@ foreach(@ARGV) {
             }
             if ($i == 10000) { last; }
         }
+    
     }
+    $pm->finish;
 }
+$pm->wait_all_children;
